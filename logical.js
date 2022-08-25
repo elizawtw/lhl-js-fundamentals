f4c93ccb091a256a286271f5b16b9cc7e15a0980
Eliza Wong <twew02@gmail.com>
2022-08-25 23:16:15 +0000

    use logical operators

---
+++ logical.js
@@ -0,0 +1,10 @@
+const isCitizen = false;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+else {
+  console.log("You are not eligible to vote.");
+}
+
