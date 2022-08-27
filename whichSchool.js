0848d92486d064dccbee116b4196c57a5ff91104
Eliza Wong <twew02@gmail.com>
2022-08-27 00:13:43 +0000

    use conditionals and while loop

---
+++ whichSchool.js
@@ -0,0 +1,15 @@
+function whichSchool (age) {  //const whichSchool = function (age){}
+  if (age < 13) {
+    return ("Elementary School");
+  }
+  else if (age >= 13 && age <= 18) {
+    return ("Secondary School");
+  }
+  else {
+    return ("Lighthouse Labs");
+  }
+}
+
+console.log(whichSchool(7));
+console.log(whichSchool(22));
+console.log(whichSchool(15));
